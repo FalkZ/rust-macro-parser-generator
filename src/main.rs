936b61c201049bbbbd20e5071fb1eabbf8041b34
Falk Zwimpfer <24669719+FalkZ@users.noreply.github.com>
2022-11-29 20:57:13 +0100

    fixed tests

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-//#![allow(dead_code, unused_imports)]
+#![allow(dead_code, unused_imports)]
 
 mod m1n;
 mod parser_generator;
-
-
 
 fn main() {}
 
 #[test]
 fn test_enum() {
-    let r = render("./examples/m1n/Enum.m1n").expect("couldn't compile file");
+    let r = crate::m1n::renderer::render("./examples/m1n/Enum.m1n").expect("couldn't compile file");
 
     insta::assert_debug_snapshot!(&r.tokens);
     insta::assert_debug_snapshot!(&r.statements);
@@ -18,7 +16,8 @@ fn test_enum() {
 
 #[test]
 fn test_class() {
-    let r = render("./examples/m1n/Class.m1n").expect("couldn't compile file");
+    let r =
+        crate::m1n::renderer::render("./examples/m1n/Class.m1n").expect("couldn't compile file");
 
     insta::assert_debug_snapshot!(&r.tokens);
     insta::assert_debug_snapshot!(&r.statements);
@@ -27,7 +26,7 @@ fn test_class() {
 
 #[test]
 fn test_file() {
-    let r = render("./examples/m1n/file.m1n").expect("couldn't compile file");
+    let r = crate::m1n::renderer::render("./examples/m1n/file.m1n").expect("couldn't compile file");
 
     insta::assert_debug_snapshot!(&r.tokens);
     insta::assert_debug_snapshot!(&r.statements);
@@ -36,7 +35,8 @@ fn test_file() {
 
 #[test]
 fn test_math() {
-    let r = render("./examples/m1n/std/math.m1n").expect("couldn't compile file");
+    let r =
+        crate::m1n::renderer::render("./examples/m1n/std/math.m1n").expect("couldn't compile file");
 
     insta::assert_debug_snapshot!(&r.tokens);
     insta::assert_debug_snapshot!(&r.statements);
