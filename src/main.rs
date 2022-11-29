b968a7772af8d615b517463bf3531a0cb59e9f83
Falk Zwimpfer <24669719+FalkZ@users.noreply.github.com>
2022-11-29 21:37:04 +0100

    test module

--- src/main.rs
+++ src/main.rs
@@ -2,48 +2,6 @@
 
 mod m1n;
 mod parser_generator;
+mod tests;
 
 fn main() {}
-
-// This is a simple macro named `say_hello`.
-macro_rules! tests {
-    (@debug: $name:ident, $t:ident, $str:literal) => {
-        #[test]
-        fn $name() {
-            let r = crate::m1n::renderer::render($str).expect("couldn't compile file");
-            insta::assert_debug_snapshot!(&r.$t);
-        }
-    };
-    (@display: $name:ident, $t:ident, $str:literal) => {
-        #[test]
-        fn $name() {
-            let r = crate::m1n::renderer::render($str).expect("couldn't compile file");
-            insta::assert_display_snapshot!(&r.$t);
-        }
-    };
-    // `()` indicates that the macro takes no argument.
-    ($($name:ident: $str:literal),*) => {
-        mod tokens {
-            $(
-                tests!(@debug: $name, tokens, $str);
-            )*
-        }
-        mod statements {
-            $(
-                tests!(@debug: $name, statements, $str);
-            )*
-        }
-        mod typescript {
-            $(
-                tests!(@display: $name, typescript, $str);
-            )*
-        }
-    };
-}
-
-tests!(
-    enums: "./examples/m1n/Enum.m1n",
-    class: "./examples/m1n/Class.m1n",
-    file: "./examples/m1n/file.m1n",
-    math: "./examples/m1n/std/math.m1n"
-);
